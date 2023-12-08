bab1b0608e8345308fe53abb7a7fe3711c998fe2
caio <zhj380@gmail.com>
2023-12-08 11:46:44 -0300

    caio

--- react-demo/src/config/dev.ts
+++ react-demo/src/config/dev.ts
@@ -1,12 +1,12 @@
 import { getExploreName } from '../utils/platform';
 
 export const devConfig = {
-  sdkKey: '',
-  sdkSecret: '',
+  sdkKey: 'aL_YCVB1SU-v_C4swreOIA',
+  sdkSecret: 'QnaMpdfWWzm6Y29FYgo20d6O5DdUlah8zcgN',
   webEndpoint: 'zoom.us',
-  topic: '',
+  topic: 'testando',
   name: `${getExploreName()}-${Math.floor(Math.random() * 1000)}`,
-  password: '',
+  password: '1234',
   signature: '',
   sessionKey: '',
   userIdentity: '',
